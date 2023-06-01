4f1c59c6ea5853b40a5bfa9cc7951aebae9ee7b6
Priya Prakash <priyprakashpandey@gmail.com>
2023-06-02 04:45:30 +0530

    Setting local server

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("Let's Start jay mata di ")
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+
+dotenv.config();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ message: "Woking Fine" });
+});
+
+//Error handlling
+app.use((error, req, res, next) => {
+  const errorMessage = error.message || "Something went wrong";
+  const errorStatus = error.status || 500;
+
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`.gray.bold);
+});
